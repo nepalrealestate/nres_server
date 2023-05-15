946bd03f272ff38128a894417480a9b5b11dc619
Amir Poudel <amirpoudel2058@gmail.com>
2023-05-15 22:53:16 +0545

    upload identification image on agent side

--- routes/users/route.agent.js
+++ routes/users/route.agent.js
@@ -3,9 +3,12 @@ const { handleAgentRegistration,handleGetAgent,handleAgentLogin, handleAgentPass
 
 const router  = express.Router();
 
+const {UploadImage} = require("../../middlewares/middleware.uploadFile");
+const path  = 'uploads/users/agent/'  //path from source 
+const uploadAgentImage = new UploadImage(path);
 
 router.get("/",handleGetAgent);
-router.post("/register",handleAgentRegistration);
+router.post("/register",uploadAgentImage.upload.array('image',2),handleAgentRegistration);
 router.post("/login",handleAgentLogin);
 router.put("/password",handleAgentPasswordReset);
 
