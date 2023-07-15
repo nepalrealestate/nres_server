d79406c149edde3498de48fd0ac1986e12d317b4
Amir Poudel <amirpoudel2058@gmail.com>
2023-07-16 00:31:30 +0545

    integration with customer chat box only success

--- controllers/chat/controller.userChat.js
+++ controllers/chat/controller.userChat.js
@@ -14,6 +14,7 @@ const handleUserChat = async function (userChat, socket) {
 
   console.log("User COnnected to user Chat");
   const userID = socket.handshake.query.sender_id;
+  console.log("User ID is " + userID)
 
   //  if user present in nres_chat.customer_list or 
   //  user successfully insert in nres_chat.customer_list 
@@ -39,14 +40,25 @@ const handleUserChat = async function (userChat, socket) {
   }
 
   userToSocket.get(userID).add(socket.id);
+  console.log(userToSocket)
 
 // after user connect get all previous chats
-  try {
-    const chat = await getSingleCustomerChat(userID);
-    socket.send(chat);
-  } catch (error) {
-    socket.send(error);
-  }
+  // try {
+  //   const chat = await getSingleCustomerChat(userID);
+  //   socket.emit("previousMessage",chat);
+  // } catch (error) {
+  //  // socket.send(error);
+  // }
+
+  socket.on("previousMessage",async function (){
+    try {
+      const chat = await getSingleCustomerChat(userID);
+      socket.emit("previousMessage",chat);
+    } catch (error) {
+      //socket.send(error);
+    }
+  })
+
 
   socket.on("message", async function (payload) {
     //save all chats to database;
@@ -54,10 +66,13 @@ const handleUserChat = async function (userChat, socket) {
     console.log("This is sender id - "+userID);
     let sender_id = userID;
     //nres - admin denoted as number 0 
-    let receiver_id = sender_id === Number(0)? 0 : payload.receiver_id;
+    let receiver_id = sender_id == 0 ? payload.receiver_id : 0 ;
      
     let message = payload.message;
     console.log(sender_id,receiver_id,message)
+    if(message.length ===0){
+      return;
+    }
 
     try {
       const response = await insertCustomerChat(
@@ -68,11 +83,18 @@ const handleUserChat = async function (userChat, socket) {
 
       if (response.affectedRows !== 0) {
         // if receiver_id present in online User then  send message
-        if (userToSocket[receiver_id]) {
-          userToSocket[receiver_id].forEach(function (socketID) {
+     
+        if (userToSocket.has(receiver_id.toString())) {
+          //send to sender
+          userToSocket.get(sender_id.toString()).forEach(function(socketID){
+            userChat.to(socketID).emit("message",{ sender_id: sender_id,receiver_id:receiver_id,message: message , timestamp:new Date().toISOString()})
+          })
+
+          //send to receiver
+          userToSocket.get(receiver_id.toString()).forEach(function (socketID) {
             userChat
               .to(socketID)
-              .emit("message", { sender_id: sender_id, message: message });
+              .emit("message", { sender_id: sender_id, receiver_id:receiver_id,message: message ,timestamp:new Date().toISOString()});
           });
         }
       }
