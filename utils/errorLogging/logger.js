36f7e5272ec5e7d59812865f78c13b3be175d644
Amir Poudel <amirpoudel2058@gmail.com>
2023-11-29 12:17:22 +0545

    add debug level to logger

--- utils/errorLogging/logger.js
+++ utils/errorLogging/logger.js
@@ -2,7 +2,7 @@
 const winston = require('winston');
 
 const logger = winston.createLogger({
-  level: 'info',
+  level: 'debug',
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
   transports: [
